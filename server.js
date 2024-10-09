f70b8b1204e9c17a98eabd0d8ac2f9fc8f127301
samba8897 <sambasivarao3521@gmail.com>
2024-10-09 14:08:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
