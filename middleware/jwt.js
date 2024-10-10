42f1ff77dd713866adc21c86cacbf5291350046a
samba8897 <sambasivarao3521@gmail.com>
2024-10-10 21:00:06 +0530

    Update jwt.js

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,45 +1,16 @@
-import axios from "axios";
-import getCurrentUser from "./getCurrentUser"; // Adjust the path accordingly
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-const newRequest = axios.create({
-  baseURL: "https://prolancer-backend.onrender.com/api/",
-  withCredentials: true,
-});
-
-newRequest.interceptors.request.use(
-  (config) => {
-    const currentUser = getCurrentUser();
-
-    
-    if (currentUser) {
-     
-      config.headers.Authorization = `Bearer ${currentUser.token || ''}`; 
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-export default newRequest;
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) return next(createError(401,"You are not authenticated!"))
 
 
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return next(createError(403,"Token is not valid!"))
+    req.userId = payload.id;
+    req.isSeller = payload.isSeller;
+    next()
+  });
+};
 
-
-// import jwt from "jsonwebtoken";
-// import createError from "../utils/createError.js";
-
-// export const verifyToken = (req, res, next) => {
-//   const token = req.cookies.accessToken;
-//   if (!token) return next(createError(401,"You are not authenticated!"))
-
-
-//   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-//     if (err) return next(createError(403,"Token is not valid!"))
-//     req.userId = payload.id;
-//     req.isSeller = payload.isSeller;
-//     next()
-//   });
-// };
